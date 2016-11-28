e046d50a6a7d96f679b3ce643b26b2d5ec16a5ea
renatock <renatockurosaki@gmail.com>
2016-11-28 15:09:38 -0200

    Atualização do controller
    Criadas as views header e footer

---
+++ application/views/footer.php
@@ -0,0 +1,10 @@
+ <footer class="page-footer light-blue">
+    <div class="container">
+      
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="orange-text text-lighten-3" href="http://www.sorocaba.ufscar.br/ufscar/">UFSCar - Sorocaba</a>
+      </div>
+    </div>
+  </footer>
--- application/views/index.php
+++ application/views/index.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>Starter Template - Materialize</title>
 
-  <!-- CSS  -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-</head>
+
 <body background="grey lighten-1">
   <nav class="light-blue lighten-1" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Testes em Aeronaves</a>
@@ -62,16 +52,6 @@
   </body>
 
 
-  <footer class="page-footer light-blue">
-    <div class="container">
-      
-    </div>
-    <div class="footer-copyright">
-      <div class="container">
-      Made by <a class="orange-text text-lighten-3" href="http://www.sorocaba.ufscar.br/ufscar/">UFSCar - Sorocaba</a>
-      </div>
-    </div>
-  </footer>
 
 
   <!--  Scripts-->

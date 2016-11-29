9483d2f065b6d46a1e76d76014885326c46e964d
RenatoAr <renato.rizzo@dcomp.sor.ufscar.br>
2016-11-29 12:56:11 -0200

    Criação dos controllers
    Criados os Controllers de Cadastro e Consulta

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,7 +4,8 @@
     </div>
     <div class="footer-copyright">
       <div class="container">
-      Made by <a class="orange-text text-lighten-3" href="http://www.sorocaba.ufscar.br/ufscar/">UFSCar - Sorocaba</a>
+      Made by <a class="orange-text text-lighten-3" href="http://www.sorocaba.ufscar.br/ufscar/">UFSCar - Sorocaba</a> Using Materialize
+      Powered by CodeIgniter
       </div>
     </div>
   </footer>
---
+++ application/views/pages/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+
+<p>Página Inicial.</p>
+
+</body>
+</html>

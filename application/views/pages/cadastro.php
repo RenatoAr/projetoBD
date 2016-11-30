a8e7d10a9db892cd0e8ae3f95d1d2b89fc705d81
renatock <renatockurosaki@gmail.com>
2016-11-30 14:30:17 -0200

    Atualização view Consulta e Cadastro

--- application/views/pages/cadastro.php
+++ application/views/pages/cadastro.php
@@ -4,15 +4,15 @@
       <form class="col s12">
         <div class="row">
           <div class="input-field col l4">
-            <input id="first_name" type="text" class="validate">
+            <input id="first_name" type="text" class="validate" name="id_aeronave">
             <label for="first_name">ID Aeronave</label>
           </div>
           <div class="input-field col l4">
-            <input id="last_name" type="text" class="validate">
+            <input id="last_name" type="text" class="validate" name="data">
             <label for="last_name">Data</label>
           </div>
           <div class="input-field col l4">
-            <input  id="disabled" type="text" class="validate">
+            <input  id="disabled" type="text" class="validate" name="id_tecnico">
             <label for="disabled">ID Técnico</label>
           </div>
         </div>
@@ -30,55 +30,55 @@
           <tr>
             <td>Pneu</td>
             <td><div class="input-field col l6">
-            <input id="last_name" type="text" class="validate">
+            <input id="last_name" type="text" class="validate" name="pontos_pneu">
             <label for="last_name">pontos</label>
           </div></td>
             <td><div class="input-field col l6">
-            <input id="last_name" type="text" class="validate">
+            <input id="last_name" type="text" class="validate" name="horas_despendidas_pneu">
             <label for="last_name">hh:mm</label>
           </div></td>
           </tr>
           <tr>
             <td>Elétrica</td>
             <td><div class="input-field col l6">
-            <input id="last_name" type="text" class="validate">
+            <input id="last_name" type="text" class="validate" name="pontos_eletrica">
             <label for="last_name">pontos</label>
           </div></td>
             <td><div class="input-field col l6">
-            <input id="last_name" type="text" class="validate">
+            <input id="last_name" type="text" class="validate" name="horas_despendidas_eletrica">
             <label for="last_name">hh:mm</label>
           </div></td>
           </tr>
           <tr>
             <td>Asas</td>
             <td><div class="input-field col l6">
-            <input id="last_name" type="text" class="validate">
+            <input id="last_name" type="text" class="validate" name="pontos_asas">
             <label for="last_name">pontos</label>
           </div></td>
             <td><div class="input-field col l6">
-            <input id="last_name" type="text" class="validate">
+            <input id="last_name" type="text" class="validate" name="horas_despendidas_asas">
             <label for="last_name">hh:mm</label>
           </div></td>
           </tr>
           <tr>
             <td>Turbinas</td>
             <td><div class="input-field col l6">
-            <input id="last_name" type="text" class="validate">
+            <input id="last_name" type="text" class="validate" name="pontos_turbinas">
             <label for="last_name">pontos</label>
           </div></td>
             <td><div class="input-field col l6">
-            <input id="last_name" type="text" class="validate">
+            <input id="last_name" type="text" class="validate" name="horas_despendidas_turbinas">
             <label for="last_name">hh:mm</label>
           </div></td>
           </tr>
           <tr>
             <td>Motores</td>
             <td><div class="input-field col l6">
-            <input id="last_name" type="text" class="validate">
+            <input id="last_name" type="text" class="validate" name="pontos_motores">
             <label for="last_name">pontos</label>
           </div></td>
             <td><div class="input-field col l6">
-            <input id="last_name" type="text" class="validate">
+            <input id="last_name" type="text" class="validate" name="horas_despendidas_motores">
             <label for="last_name">hh:mm</label>
           </div></td>
           </tr>
--- application/views/pages/consulta.php
+++ application/views/pages/consulta.php
@@ -3,37 +3,37 @@
 
     <div class="section no-pad-bot" id="index-banner" >
 
-    
-      <div class="row" >
-        <form class="col l12" action="<?=base_url('index.php/consulta') ?>" target="_blank">
-          <div class="row">
-            <div class="input-field col l3">
-              <i class="material-icons prefix">account_circle</i>
-              <input id="icon_prefix" name="aeronave" type="text" class="validate">
-              <label for="icon_prefix">Aeronave</label>
+      <div class="container">
+        <div class="row" >
+          <form class="col l12" action="<?=base_url('index.php/consulta') ?>" target="_blank">
+            <div class="row">
+              <div class="input-field col l3">
+                <i class="material-icons prefix">account_circle</i>
+                <input id="icon_prefix" name="aeronave" type="text" class="validate">
+                <label for="icon_prefix">Aeronave</label>
+              </div>
+              <div class="input-field col l3">
+                <i class="material-icons prefix">phone</i>
+                <input id="icon_telephone" name="Teste" type="tel" class="validate">
+                <label for="icon_telephone">Teste</label>
+              </div>
+              <div class="input-field col l3">
+                <i class="material-icons prefix">phone</i>
+                <input id="icon_telephone" name="inspecao" type="tel" class="validate">
+                <label for="icon_telephone">Inspeção</label>
+              </div>
+              <div class="input-field col l3">
+                <i class="material-icons prefix">phone</i>
+                <input id="icon_telephone" name="tecnico" type="tel" class="validate">
+                <label for="icon_telephone">Técnico</label>
+              </div>
+              <div>
+                <input type="submit" value="Buscar" class="waves-effect waves-light btn blue lighten-1">              
+              </div>
             </div>
-            <div class="input-field col l3">
-              <i class="material-icons prefix">phone</i>
-              <input id="icon_telephone" name="Teste" type="tel" class="validate">
-              <label for="icon_telephone">Teste</label>
-            </div>
-            <div class="input-field col l3">
-              <i class="material-icons prefix">phone</i>
-              <input id="icon_telephone" name="inspecao" type="tel" class="validate">
-              <label for="icon_telephone">Inspeção</label>
-            </div>
-            <div class="input-field col l3">
-              <i class="material-icons prefix">phone</i>
-              <input id="icon_telephone" name="tecnico" type="tel" class="validate">
-              <label for="icon_telephone">Técnico</label>
-            </div>
-            <div>
-              <input type="submit" value="Buscar" class="waves-effect waves-light btn">              
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    
 
 
 

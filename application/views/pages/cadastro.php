8fe848b703b735bc5a476a980e040f5265e0fd57
renatock <renatockurosaki@gmail.com>
2016-11-30 14:17:58 -0200

    Criada a tela de cadastro

---
+++ application/views/pages/cadastro.php
@@ -0,0 +1,90 @@
+<div class="container">
+  <h4>Cadastro</h4>
+  <div class="row">
+      <form class="col s12">
+        <div class="row">
+          <div class="input-field col l4">
+            <input id="first_name" type="text" class="validate">
+            <label for="first_name">ID Aeronave</label>
+          </div>
+          <div class="input-field col l4">
+            <input id="last_name" type="text" class="validate">
+            <label for="last_name">Data</label>
+          </div>
+          <div class="input-field col l4">
+            <input  id="disabled" type="text" class="validate">
+            <label for="disabled">ID Técnico</label>
+          </div>
+        </div>
+
+        <table class="highlight">
+        <thead>
+          <tr>
+              <th data-field="id">Testes</th>
+              <th data-field="name">Pontuação</th>
+              <th data-field="price">Horas Despendidas</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Pneu</td>
+            <td><div class="input-field col l6">
+            <input id="last_name" type="text" class="validate">
+            <label for="last_name">pontos</label>
+          </div></td>
+            <td><div class="input-field col l6">
+            <input id="last_name" type="text" class="validate">
+            <label for="last_name">hh:mm</label>
+          </div></td>
+          </tr>
+          <tr>
+            <td>Elétrica</td>
+            <td><div class="input-field col l6">
+            <input id="last_name" type="text" class="validate">
+            <label for="last_name">pontos</label>
+          </div></td>
+            <td><div class="input-field col l6">
+            <input id="last_name" type="text" class="validate">
+            <label for="last_name">hh:mm</label>
+          </div></td>
+          </tr>
+          <tr>
+            <td>Asas</td>
+            <td><div class="input-field col l6">
+            <input id="last_name" type="text" class="validate">
+            <label for="last_name">pontos</label>
+          </div></td>
+            <td><div class="input-field col l6">
+            <input id="last_name" type="text" class="validate">
+            <label for="last_name">hh:mm</label>
+          </div></td>
+          </tr>
+          <tr>
+            <td>Turbinas</td>
+            <td><div class="input-field col l6">
+            <input id="last_name" type="text" class="validate">
+            <label for="last_name">pontos</label>
+          </div></td>
+            <td><div class="input-field col l6">
+            <input id="last_name" type="text" class="validate">
+            <label for="last_name">hh:mm</label>
+          </div></td>
+          </tr>
+          <tr>
+            <td>Motores</td>
+            <td><div class="input-field col l6">
+            <input id="last_name" type="text" class="validate">
+            <label for="last_name">pontos</label>
+          </div></td>
+            <td><div class="input-field col l6">
+            <input id="last_name" type="text" class="validate">
+            <label for="last_name">hh:mm</label>
+          </div></td>
+          </tr>
+        </tbody>
+      </table>
+
+      </form>
+    </div>
+</div>
